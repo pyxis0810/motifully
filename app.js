82bb63c2d884f86cf805df24f995e31ce85b72f0
1002627 <pyxis@sk.com>
2016-01-22 11:38:10 +0900

    gulp configuration

--- app.js
+++ app.js
@@ -10,6 +10,10 @@ var users = require('./routes/users');
 
 var app = express();
 
+app.set('views', path.join(__dirname, 'src'));
+app.set('view engine', 'html');
+app.engine('html', require('ejs').renderFile);
+
 // uncomment after placing your favicon in /public
 //app.use(favicon(path.join(__dirname, 'public', 'favicon.ico')));
 app.use(logger('dev'));
--- gulpfile.js
+++ gulpfile.js
@@ -3,33 +3,34 @@ var gulp = require('gulp'),
     webserver = require('gulp-webserver'),
     compass = require('gulp-compass'),
     browserSync = require('browser-sync').create(),
+    nodemon = require('gulp-nodemon'),
     config = require('./config');
 
-gulp.task('serve', ['compass', 'webserver'], function () {
+gulp.task('default', ['browser-sync'], function () {
 
 });
 
-gulp.task('dev', ['compass', 'browser-sync'], function () {
-
+gulp.task('browser-sync', ['nodemon'], function() {
+    browserSync.init(null, {
+        proxy: "http://localhost:3000",
+        files: ["src/**/*.*"],
+        browser: "google chrome",
+        port: 8080,
+    });
 });
 
-gulp.task('compass', function () {
-    return sass('src/sass/*.sass')
-        .on('error', sass.logError)
-        .pipe(gulp.dest('public/css'));
-});
-
-gulp.task('webserver', function () {
-    gulp.src('.')
-        .pipe(webserver({
-            host: 'localhost',
-            port: 3000,
-            fallback: 'index.html',
-            livereload: true,
-            open: true
-        }));
-});
-
-gulp.task('devserver', ['sass'], function () {
-    browserSync.init(config.options.browserSync);
+gulp.task('nodemon', function (cb) {
+    
+    var started = false;
+    
+    return nodemon({
+        script: 'bin/www'
+    }).on('start', function () {
+        // to avoid nodemon being started multiple times
+        // thanks @matthisk
+        if (!started) {
+            cb();
+            started = true; 
+        } 
+    });
 });
