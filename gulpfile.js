d1484f2e393164c5405ffd37314d5de9677c4308
1002627 <pyxis@sk.com>
2016-01-22 13:20:06 +0900

    express configuration

--- gulpfile.js
+++ gulpfile.js
@@ -12,10 +12,10 @@ gulp.task('default', ['browser-sync'], function () {
 
 gulp.task('browser-sync', ['nodemon'], function() {
     browserSync.init(null, {
-        proxy: "http://localhost:3000",
+        proxy: "http://localhost:8000",
         files: ["src/**/*.*"],
         browser: "google chrome",
-        port: 8080,
+        port: 3000
     });
 });
 
@@ -27,7 +27,6 @@ gulp.task('nodemon', function (cb) {
         script: 'bin/www'
     }).on('start', function () {
         // to avoid nodemon being started multiple times
-        // thanks @matthisk
         if (!started) {
             cb();
             started = true; 
